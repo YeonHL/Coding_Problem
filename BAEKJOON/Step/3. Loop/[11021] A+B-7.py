b34e27da1fef7d3520ef1deef7386d5b9737dd6b
kitearc <Yeonholee.dev@gmail.com>
2022-10-30 12:03:42 +0900

    clean push

--- BAEKJOON/Step/3. Loop/[11021] A+B-7.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-i = int(sys.stdin.readline().rstrip())
-
-for x in range(1, i+1):
-    a, b = sys.stdin.readline().rstrip().split()
-    a = int(a)
-    b = int(b)
-    print("Case #%d:" % x, a+b)
